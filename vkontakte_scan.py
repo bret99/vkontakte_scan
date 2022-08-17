ccbcd1de6ad407c247b9e99de61d678163bf67aa
bret99 <64787351+bret99@users.noreply.github.com>
2022-08-17 15:56:05 +0000

    Update vkontakte_scan.py

--- vkontakte_scan.py
+++ vkontakte_scan.py
@@ -12,35 +12,37 @@ positive_answers = ['yes', 'y', '']
 
 
 def main_menu():
-    print(
-        "\033[1;94m\nVK Scanner \033[1;00mmodules:\n\n[\033[1;94m1\033[1;00m] Get target \033[1;94mFriends\n\033[1;00m[\033[1;94m2\033[1;00m] Get target \033[1;94mFollowers\n\033[1;00m[\033[1;94m3\033[1;00m] Get target \033[1;94mSubscriptions\n\033[1;00m[\033[1;94m4\033[1;00m] Get target \033[1;94mGroups\n\033[1;00m[\033[1;94m5\033[1;00m] Get target \033[1;94mPhotos\033[1;00m\n[\033[1;94m6\033[1;00m] Get target \033[1;94mPosts\033[1;00m\n[\033[1;94m7\033[1;00m]\033[1;94m Search \033[1;00mquery in target \033[1;94mFriends\033[1;00m\n[\033[1;94m99\033[1;00m] \033[1;90mExit\033[1;00m\n"
-    )
-    choose_module = input("Enter module number: ")
-    if choose_module == "1":
-        Friends()
-        main_menu()
-    elif choose_module == "2":
-        Followers()
-        main_menu()
-    elif choose_module == "3":
-        Subscriptions()
-        main_menu()
-    elif choose_module == "4":
-        Groups()
-        main_menu()
-    elif choose_module == "5":
-        Photos()
-        main_menu()
-    elif choose_module == "6":
-        Posts()
-        main_menu()
-    elif choose_module == "7":
-        Search()
-        main_menu()
-    elif choose_module == "99":
-        sys.exit()
-    else:
-        sys.exit("\033[1;91mWrong input!\033[1;00m")
-
+    try:
+        print(
+            "\033[1;94m\nVK Scanner \033[1;00mmodules:\n\n[\033[1;94m1\033[1;00m] Get target \033[1;94mFriends\n\033[1;00m[\033[1;94m2\033[1;00m] Get target \033[1;94mFollowers\n\033[1;00m[\033[1;94m3\033[1;00m] Get target \033[1;94mSubscriptions\n\033[1;00m[\033[1;94m4\033[1;00m] Get target \033[1;94mGroups\n\033[1;00m[\033[1;94m5\033[1;00m] Get target \033[1;94mPhotos\033[1;00m\n[\033[1;94m6\033[1;00m] Get target \033[1;94mPosts\033[1;00m\n[\033[1;94m7\033[1;00m]\033[1;94m Search \033[1;00mquery in target \033[1;94mFriends\033[1;00m\n[\033[1;94m99\033[1;00m] \033[1;90mExit\033[1;00m\n"
+        )
+        choose_module = input("Enter module number: ")
+        if choose_module == "1":
+            Friends()
+            main_menu()
+        elif choose_module == "2":
+            Followers()
+            main_menu()
+        elif choose_module == "3":
+            Subscriptions()
+            main_menu()
+        elif choose_module == "4":
+            Groups()
+            main_menu()
+        elif choose_module == "5":
+            Photos()
+            main_menu()
+        elif choose_module == "6":
+            Posts()
+            main_menu()
+        elif choose_module == "7":
+            Search()
+            main_menu()
+        elif choose_module == "99":
+            sys.exit()
+        else:
+            sys.exit("\033[1;91mWrong input!\033[1;00m")
+    except KeyboardInterrupt:
+        sys.exit("\n")
 
 main_menu()
